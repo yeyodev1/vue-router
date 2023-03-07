58e596b25d44336a455b32bd7e93afc3dc841d22
Diego Reyes <diegorele13@gmail.com>
2023-03-07 14:01:27 -0500

    routs with names

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,25 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'home',
       component: HomeView
     },
     {
+      path: '/session',
+      component: () => import('../views/SessionView.vue'),
+      children: [
+        {
+          path: '',
+          components: {
+            default: () => import('../views/LoginView.vue'),
+            register: () => import('../views/RegisterView.vue')
+          }
+        }
+      ]
+    },
+    {
       path: '/about',
+      name: 'about',
       component: () => import('../views/AboutView.vue')
     },
     {
